17e854d53fcb9e386c456da83d59ddfdd2dc467e
VZ <vitali.zaikov@gmail.com>
2020-09-03 15:02:07 +0300

    1 задание

---
+++ controller/controller.php
@@ -0,0 +1,53 @@
+<?php
+
+class Controller {
+
+	public static function StartSite() {
+		$arr = News::getLast10News();
+		include_once 'view/start.php';
+	}
+
+	public static function AllCategory() {
+		$arr = Category::getAllCategory();
+		include_once 'view/category.php';
+	}
+
+	public static function AllNews() {
+		$arr = News::getAllNews();
+		include_once 'view/allnews.php';
+	}
+
+	public static function NewsByID($id) {
+	    $n = News::getNewsByID($id);
+	    include_once 'view/readnews.php';
+	}
+
+	    public static function error404() {
+	    include_once 'view/error404.php';
+	}
+
+	    public static function InsertComment($c,$id)
+	    Comments::InsertComment($c,$id);
+	    //self::NewsByID($id);
+	    header('Location:news?id='.$id.'#ctable');
+	}
+
+	//список комментариев
+	    public static function Comments($newsid) {
+	    	$arr = Comments::getCommentByNewsID($newsid);
+	    	ViewComments::CommentsByNews($arr);
+
+	    }
+	    //количество комментариев к новости
+	    public static function CommentsCount($newsid) {
+	    	$arr = Comments::getCommentCount($newsid) {
+	    	ViewComments::CommentsCount($arr);
+	    }
+	    //ссылка - переход к списку комментариев
+	    public static function CommentsCountWithAncor($newsid) {
+	    	$arr = Comments::getCommentCountByNewsID($newsid);
+	    	ViewComments::CommentsCountWithAncor($arr);
+	    }
+
+
+	//end class
